ae584841a5be9f1196fb89c861a58a19f07ad77e
vizshrc <vizshrc@gmail.com>
2020-11-04 20:17:57 +0800

    2

--- docker_install.sh
+++ docker_install.sh
@@ -50,6 +50,7 @@ locaton_select(){
     1.国外 )
 #错误则需要sudo包，2>1&重定向错误信息 丢弃
       (sudo -h >> /dev/null 2>&1) || ${apt} install sudo
+      (curl -h >> /dev/null 2>&1) || ${apt} install curl
       curl -fsSL https://get.docker.com | bash -s docker
       docker -v&&echo_GreenFont "docker已经就绪"||echo_RedFont "请检查出错"
       
@@ -57,6 +58,7 @@ locaton_select(){
       ;;
     2.国内 )
       (sudo -h >> /dev/null 2>&1) || ${apt} install sudo
+      (curl -h >> /dev/null 2>&1) || ${apt} install curl
       curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
       docker -v&&echo_GreenFont "docker已经就绪"||echo_RedFont "请检查出错"
       
